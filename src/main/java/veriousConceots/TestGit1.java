1cc2a4ae45a5d7ff86e56dd709e14f9cc7586b5b
Al <Al@alSAdeq.com>
2021-11-17 11:14:49 -0500

    third commit

---
+++ src/main/java/veriousConceots/TestGit1.java
@@ -0,0 +1,19 @@
+package veriousConceots;
+
+public class TestGit1 {
+
+	public static void main(String[] args) {
+		printMe();
+		printMe1();
+	}	
+		
+		public static void printMe() {
+		
+		System.out.println("test git");
+		
+	}
+		public static void printMe1() {
+			
+			System.out.println("test git1");
+	}}
+
